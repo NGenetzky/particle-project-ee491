d30b2b2998e02156ae5809fbc93feb71fdf33ab0
Nathan Genetzky <nathan@genetzky.us>
2018-11-14 00:47:11 -0600

    ee491: Copy files board.cpp,board.h,global.h

--- firmware/board.cpp
+++ firmware/board.cpp
@@ -1,1 +1,56 @@
-../../firmware/board/board.cpp
+
+#include "board.h"
+#include "application.h"
+#include "global.h"
+
+int led_on(){
+    digitalWrite(BOARD_LED, HIGH);
+    return 0;
+}
+
+int led_off(){
+    digitalWrite(BOARD_LED, LOW);
+    return 0;
+}
+
+int reset_board(){
+    System.reset();
+    return 0;
+}
+
+void disable_cloud_ifD6() {
+    pinMode(D6, INPUT); //INPUT, INPUT_PULLUP, INPUT_PULLDOWN or OUTPUT
+    if(digitalRead(D6)){  // Indicated whether cloud should be disabled.
+        if(Particle.connected() == true){
+            Particle.disconnect();
+        }
+    } else {
+        if(Particle.connected() == false){
+            Particle.connect();
+        }
+    }
+}
+
+
+//Electron does not have wifi.
+#if (defined(PLATFORM_ID) && ((PLATFORM_ID == 0) || (PLATFORM_ID == 6)) )
+    //Core or Photon only.
+    void disable_wifi_ifD6() {
+        pinMode(D6, INPUT); //INPUT, INPUT_PULLUP, INPUT_PULLDOWN or OUTPUT
+        if(digitalRead(D6)){  // Indicated whether cloud should be disabled.
+            if(WiFi.ready() == true){
+                WiFi.off();
+                Particle.disconnect();
+            }
+        } else {
+            if(WiFi.ready() == false){
+                WiFi.on();
+                Particle.connect();
+            }
+        }
+    }
+#else
+    void disable_wifi_ifD6(){
+        #warning "Electron Does not have Wifi"
+    }
+#endif
--- firmware/board.h
+++ firmware/board.h
@@ -1,1 +1,14 @@
-../../firmware/board/board.h
+
+#ifndef CMD_PARTICLE_H
+#define CMD_PARTICLE_H
+
+int led_on();
+int led_off();
+int reset_board();
+void disable_cloud_ifD6();
+
+//Electron does not have wifi.
+// #if ((PLATFORM_ID == 0) || (PLATFORM_ID == 6)) //Core or Photon only.
+void disable_wifi_ifD6();
+
+#endif // CMD_PARTICLE_H
